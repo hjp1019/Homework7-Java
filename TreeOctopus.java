e8e0befbd8c39eaf6b6b1c7fe51776e87707184a
hjp1019 <70728512+hjp1019@users.noreply.github.com>
2021-03-21 14:29:26 -0400

    Add files via upload

---
+++ TreeOctopus.java
@@ -0,0 +1,16 @@
+package hw7;
+
+public class TreeClimbingOctopus {
+	public String aboutMe() {
+		return "Octopus";
+	}
+	
+	public String Iclimb() {
+		return "tree";
+	}
+	
+	public String movement() {
+		return "waddle and swim";
+	}
+
+}
